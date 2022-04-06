ec3a4350751154b2894ec035794b2492de419e58
Rey Jairus Marasigan <reymarasigan001@gmail.com>
2022-04-06 18:37:48 -0400

    add skeleton to Directory.java and Account.java

---
+++ app/Directory.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Directory
+{
+    private String name;
+    private List<Account> entries;
+
+    public Account getAccount(){ // getting an entry from the list
+
+    }
+
+    public void setAccount(Account input){ //should we pass the entire database or only specific interfaces?
+
+    }
+
+    public String getName(){
+
+    }
+
+    public void setName(String input){
+        
+    }
+}
