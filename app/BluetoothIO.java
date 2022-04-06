d9b2745412f2f9d5d2c50d14dbbf68f3aec2e850
Gentman Tan <gentmantan@gmail.com>
2022-04-06 18:46:13 -0400

    Added skeleton code for bluetoothio class

---
+++ app/BluetoothIO.java
@@ -0,0 +1,22 @@
+public class BluetoothIO
+{
+    public BluetoothIO(){
+
+    }
+
+    public ArrayList searchDongle(){
+
+    }
+
+    public void selectDongle(DongleAddress addr){
+
+    }
+
+    public void keyboardInput(String input){
+
+    }
+
+    private void pairDongle(DongleAddress addr){
+
+    }
+}
