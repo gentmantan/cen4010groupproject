2674fa8514cf33adc3f5448419e5dbc9a1b5ca70
Rey Jairus Marasigan <reymarasigan001@gmail.com>
2022-04-06 19:04:56 -0400

    finished skeleton on account.java, directory.java, database.java

--- app/Database.java
+++ app/Database.java
@@ -1,4 +1,22 @@
 public class Database
 {
+    private List<Directory> dirs;
+    private EncryptedFile encryptedFile;
+
+    public Directory getDirectory(int index){
+
+    }
+
+    public void addDirectory(Directory input){
+
+    }
+
+    public EncryptedFile getEncryptedFile() {
+
+    }
+
+    public void setEncryptedFile(EncryptedFile input) {
+
+    }
 
 }
--- app/Directory.java
+++ app/Directory.java
@@ -3,13 +3,22 @@ import java.util.ArrayList;
 public class Directory
 {
     private String name;
+    private Database database;
     private List<Account> entries;
 
-    public Account getAccount(){ // getting an entry from the list
+    public Database getDatabase(){
 
     }
 
-    public void setAccount(Account input){ //should we pass the entire database or only specific interfaces?
+    public void setDatabase() {
+
+    }
+
+    public Account getAccount(int index){ // getting an entry from the list
+
+    }
+
+    public void addAccount(Account input){ //should we pass the entire database or only specific interfaces?
 
     }
 
