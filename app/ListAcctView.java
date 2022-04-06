34b2cc01dcc787413a4584475fa99741546be0a3
Rey Jairus Marasigan <reymarasigan001@gmail.com>
2022-04-06 19:30:41 -0400

    did ListAcctView.java and PassEntryView.java

---
+++ app/ListAcctView.java
@@ -0,0 +1,32 @@
+class ListAcctView {
+
+    private UI ui;
+
+    public UI getUI(){ 
+
+    }
+
+    private void setUI(){ 
+
+    }
+
+    public void printAcctDirName(){
+
+    }
+    
+    public void showEntry(){
+
+    }
+
+    public void enterEntry(){
+
+    }
+
+    public void inputAcct(){
+
+    }
+
+    public void jumpToLetter(){
+
+    }
+}
