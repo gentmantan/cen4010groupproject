cc3ad981ce0cd1999ee115b9444d661d27fe5b20
Gentman Tan <gentmantan@gmail.com>
2022-04-11 23:48:40 -0400

    Rewrote UI to match changes

--- app/UI.java
+++ app/UI.java
@@ -1,18 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package javaapplication55;
-
 /**
  *
  * @author Chidie17
  */
 public class UI {
-    AcctView accountView;
-    EditAcctView editView;
-    PassEntryView passView;
-    ListAcctView listAcctView;
-    AddActView addActView;
+
+    public String drawPassGenView(){
+        PassGenView passView = new PassGenView();
+        return passView.getPass();
+    }
+
+    public Account drawListAcctView(Database db){
+        ListAcctView listAcctView = new ListAcctView();
+        listAcctView.setDatabase(db);
+        return listAcctView.enterEntry();
+    }
+
+    public Account drawEditAcctView(Account acct){
+        EditAcctView editAcctView = new EditAcctView(acct);
+        return editAcctView.getAccount();
+    }
+
+    public void drawReadAcctView(Account acct){
+        ReadAcctView readAcctView = new ReadAcctView(acct);
+    }
+
+
+    public String drawDBLoginView(){
+        DBLoginView dblogin = new DBLoginView();
+        return dblogin.inputPass();
+    }
 }
